b2c54cec435f98a2de6a82afa0dcd90d80ea4230
Luis <luis.beltran@itcelaya.edu.mx>
2015-08-05 15:56:00 +0200

    Hola Xamarin
    1st commit

---
+++ HolaXamarin/PaginaHola.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HolaXamarin
+{
+	public partial class PaginaHola : ContentPage
+	{
+		public PaginaHola ()
+		{
+			InitializeComponent ();
+		}
+
+		void btnSaludar_Clicked (object sender, EventArgs e)
+		{
+			string nombre = txtNombre.Text;
+			string mensaje = String.Format ("Hola {0} desde Xamarin.Forms", nombre);
+
+			DisplayAlert ("Saludos", mensaje, "OK");
+		}
+	}
+}
